37b84a1ef9fe17b8e4c9cb5bc48a671679e23fb8
gofer <dawidmarkiewicz214@gmail.com>
2026-03-13 22:33:55 +0100

    nixvim config :3c

--- modules/dev/nixvim/default.nix
+++ modules/dev/nixvim/default.nix
@@ -1,46 +1,5 @@
-{inputs, ... }: 
-let
-  flakePath = "/home/gofer/.config/home-manager";
-in
-{
+{...}: {
   imports = [
-    inputs.nixvim.homeModules.nixvim	
+    ./nixvim.nix
   ];
-
-  programs.nixvim = {
-    enable = true;
-    defaultEditor = true;
-
-    colorschemes.catppuccin.enable = true;
-
-    globals.mapleader = " ";
-
-    plugins = {
-      lualine.enable = true;
-      treesitter.enable = true;
-      oil.enable = true;
-      
-      blink-cmp = {
-        enable = true;
-        settings.sources.default = [ "lsp" "path" "snippets" "buffer" ];
-      };
-
-      lsp = {
-        enable = true;
-        servers = {
-          lua_ls.enable = true;
-          gopls.enable = true;
-          jdtls.enable = true;
-          nixd = {
-            enable = true;
-            settings.options = {
-              nixos.expr = "(builtins.getFlake \"${flakePath}\").nixosConfigurations.szkolny.options";
-              home_manager.expr = "(builtins.getFlake \"${flakePath}\").homeConfigurations.gofer.options";
-              nixvim.expr = "(builtins.getFlake \"${flakePath}\").homeConfigurations.gofer.options.programs.nixvim";
-            };
-          };
-        };
-      };
-    };
-  };
 }
